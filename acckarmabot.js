57740a60464349f81462a762c8454e70856ff70b
Evan Lennick <elennick@gmail.com>
2016-07-30 17:01:03 -0400

    cleanup

--- acckarmabot.js
+++ acckarmabot.js
@@ -1,20 +1,17 @@
 var Botkit = require('botkit');
 var PropertiesReader = require('properties-reader');
 
-var properties = PropertiesReader('bot.properties');
-
-var token = properties.get('slack.token');
-console.log(token);
+var getSlackToken = function() {
+  var properties = PropertiesReader('bot.properties');
+  var token = properties.get('slack.token');
+  console.log("Loaded Slack token: " + token);
+  return token;
+};
 
 var controller = Botkit.slackbot();
-
 var bot = controller.spawn({
-  token: token
-});
-
-var users = [];
-
-bot.startRTM(function(err,bot,payload) {
+  token: getSlackToken()
+}).startRTM(function(err,bot,payload) {
   if (err) {
     throw new Error('Could not connect to Slack');
   }
