669ee773a549de21387a9eaab1273037d5af8771
Evan Lennick <elennick@gmail.com>
2016-08-01 08:29:57 -0400

    started messing around with karma stuff

--- acckarmabot.js
+++ acckarmabot.js
@@ -1,6 +1,10 @@
 var Botkit = require('botkit');
 var PropertiesReader = require('properties-reader');
 
+const NAME = "KarmaBot";
+
+var users = [];
+
 var getSlackToken = function() {
   var properties = PropertiesReader('bot.properties');
   var token = properties.get('slack.token');
@@ -9,16 +13,37 @@ var getSlackToken = function() {
 };
 
 var controller = Botkit.slackbot();
-var bot = controller.spawn({
-  token: getSlackToken()
-}).startRTM(function(err,bot,payload) {
-  if (err) {
-    throw new Error('Could not connect to Slack');
-  }
-});
+
+var initBot = function(controller) {
+  var bot = controller.spawn({
+    token: getSlackToken()
+  }).startRTM(function(err,bot,payload) {
+    if (err) {
+      throw new Error('Could not connect to Slack');
+    }
+  });
+  console.log("Bot initialized.");
+  return bot;
+};
+
+var bot = initBot(controller);
 
 controller.hears(["^@.*\\+\\+$"],["ambient"],function(bot,message) {
-  bot.reply(message,'You said ' + message.text);
+  bot.reply(message,{
+    text: "You said: '" + message.text + "'",
+    username: NAME,
+    icon_emoji: ":unicorn_face:",
+  });
 
-  // var username = message.text
+  var user = users[message.text];
+  console.log(user);
+  if(user === undefined) {
+    user = {};
+    user.karma = 1;
+    users[message.text] = user;
+  } else {
+    user.karma += 1;
+  }
+
+  console.log(users);
 });
